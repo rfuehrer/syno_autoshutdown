f7f9de362abc92266d227ac62531d8614123394a
Rene Fuehrer <rene.fuehrer@generali.com>
2020-08-06 06:37:21 +0200

    Fix killall script (pkill did not work reliably)

--- autoshutdown_killall.sh
+++ autoshutdown_killall.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-pkill -f autoshutdown.sh
-pkill -f autoshutdown_webserver.py
+
+SEARCH_KILL="autoshutdown.sh"
+echo "Searching for '$SEARCH_KILL' processes..."
+for p in `ps -ef|grep $SEARCH_KILL|grep -v grep| awk '{print $2}'`; do
+   echo "Killing '$SEARCH_KILL' with PID $p..."
+   kill -9 $p
+done
+
+SEARCH_KILL="autoshutdown_webserver.py"
+echo "Searching for '$SEARCH_KILL' processes..."
+for p in `ps -ef|grep $SEARCH_KILL|grep -v grep| awk '{print $2}'`; do
+   echo "Killing '$SEARCH_KILL' with PID $p..."
+   kill -9 $p
+done
